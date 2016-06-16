2d5725818e64649b6e0f6ed9d7f55e2193e3cd07
Addy Kim <soyaddyk@gmail.com>
2016-06-15 23:15:30 -0700

    Move template

--- app/models/item.js
+++ app/models/item.js
@@ -1,10 +1,8 @@
-import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import DS from 'ember-data';
 
-export default Model.extend({
-  category: attr('string'),
-  description: attr('string'),
-  weight: attr('number'),
-  rating: attr('number')
+export default DS.Model.extend({
+  category: DS.attr('string'),
+  description: DS.attr('string'),
+  weight: DS.attr('number'),
+  rating: DS.attr('number')
 });
