522237199a08018fceb7c1e1a1184e348a595afa
Addy Kim <soyaddyk@gmail.com>
2016-06-20 23:15:14 -0700

    I messed up so bad I just deleted everything

--- app/adapters/application.js
+++
@@ -1,8 +0,0 @@
-// import JSONAPIAdapter from 'ember-data/adapters/json-api';
-
-// export default JSONAPIAdapter.extend({
-// });
-
-import DS from 'ember-data';
-
-export default DS.FixtureAdapter.extend({});
